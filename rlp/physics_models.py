f847cec55f24bd56927824350644e9269062439f
Will Ballengee <william.ballengee@gmail.com>
2021-04-13 18:26:43 -0400

    updated beers law function to calculate across entire spectra, WIP

--- rlp/physics_models.py
+++ rlp/physics_models.py
@@ -1,16 +1,34 @@
-def beers_law(solution, path_length, coeffs):
+def beers_law(solution, path_length, coeffs, min_wavelength, max_wavelength):
     """
     Applys Beers-Lampert Law to get the total absorbtion
     of a solution.
     Inputs:
     solution: Dictionary of the form Name->Concentration
     path_length: float
-    coeffs: pd dataframe with two columns 'Name', 'Coefficients'
+    coeffs: pd dataframe with column 'Wavelength' and coefficients in other column.
+    Each other column corresponds to the coffiecient at that wavelength for the 
+    title of the column which should be the same as the solution
+    
+    'Wavelength', 'Dye1', 'Dye2'
+    600, .1, 0
+    601, .2, .1
 
     Returns the overall absorbtion as a float
     """
+    wavelengths = []
+    spectra = []
     absorbtion = 0
-    coeffs_with_index = coeffs.set_index('Name')
-    for key in solution:
-        absorbtion += solution[key] * coeffs_with_index.loc[key, 'Coefficient']
-    return absorbtion*path_length
+    coeffs_with_index = coeffs.set_index('Wavelength')
+    coeffs_with_index.sort_index(inplace=True)
+    coeffs_with_index.dropna(inplace=True)
+    
+    for index, row in coeffs_with_index.iterrows():
+        if index >= min_wavelength and index <= max_wavelength:
+            for key in solution:
+                absorbtion += solution[key] * coeffs_with_index.loc[index, key]
+            wavelengths.append(index)
+            spectra.append(absorbtion)
+            absorbtion = 0
+        
+    
+    return wavelengths, spectra*path_length
--- rlp/test_physics_models.py
+++ rlp/test_physics_models.py
@@ -7,15 +7,18 @@ def test_beers_law():
     test_coeffs = pd.read_csv('test_coeff.csv')
     test_solution_1 = {'A': .1, 'B': .4, 'C': 1.0}  # 'Name'->'Molarity'
     test_path_length = 1  # cm
-    res = physics_models.beers_law(test_solution_1,
-                                   test_path_length, test_coeffs)
-    expected = 1.4
-    assert math.isclose(res, expected), f'Expected {expected}, got {res} instead'
-
-    test_coeffs = pd.read_csv('test_coeff.csv')
-    test_solution_2 = {'A': 10, 'B': 0, 'C': .01}
-    test_path_length = .25
-    res = physics_models.beers_law(test_solution_2,
-                                   test_path_length, test_coeffs)
-    expected = 2.50125
-    assert math.isclose(res, expected), f'Expected {expected}, got {res} instead'
+    test_max_wl = 800
+    test_min_wl = 400
+    wl, spec = physics_models.beers_law(test_solution_1,
+                                   test_path_length, test_coeffs,
+                                   test_min_wl, test_max_wl)
+    
+    
+    expected_spec = [1.4, 1.65, 1.7]
+    expected_wl = [400, 600, 800]
+    
+    for i in range(expected):
+        assert math.isclose(wl[i], expected_wl[i]), f'Expected wavelength {expected_wl[i]}, got {wl[i]} instead'
+        assert math.isclose(spec[i], expected_spec[i]), f'Expected wavelength {expected_spec[i]}, got {spec[i]} instead'
+        
+    assert len(expected_spec) is len(spec), f'Expected results length of {len(expected_spec)}, got {len(spec)} instead'

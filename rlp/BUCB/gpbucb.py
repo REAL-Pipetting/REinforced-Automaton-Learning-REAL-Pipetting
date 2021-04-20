7d483710b8b49085fb902527c4f24b23dc65597d
Samantha Tetef <stetef@u.rochester.edu>
2021-04-20 14:51:45 -0700

    First attempt of GP-BUCB alg

---
+++ rlp/BUCB/gpbucb.py
@@ -0,0 +1,7 @@
+"""
+Implements Gaussian Process Batch Upper Confidence Bound (GP-BUCB).
+
+Based on Parallelizing Exploration-Exploitation Tradeoffs in
+Gaussian Process Bandit Optimization, Desautels, et. al, 2014.
+
+"""

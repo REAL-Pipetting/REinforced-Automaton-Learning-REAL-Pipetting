ce41944b040fd11ab69172efdbc1b8e404620943
Samantha Tetef <stetef@u.rochester.edu>
2021-04-28 13:43:45 -0700

    Implemented first draft of GP BUCB alg

--- rlp/UCB/ucb.py
+++ rlp/UCB/ucb.py
@@ -5,8 +5,6 @@ Based on Parallelizing Exploration-Exploitation Tradeoffs in
 Gaussian Process Bandit Optimization, Desautels, et. al, 2014.
 
 """
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import pylab as plt
 import numpy as np
 from sklearn.gaussian_process import GaussianProcessRegressor
 
@@ -15,52 +13,110 @@ class GPUCB(object):
     """Heavilty adapted from https://github.com/tushuhei/gpucb."""
 
     def __init__(self, meshgrid, environment, beta=100.):
-        '''
-        meshgrid: Output from np.methgrid.
-        e.g. np.meshgrid(np.arange(-1, 1, 0.1), np.arange(-1, 1, 0.1)) for 2D space
-        with |x_i| < 1 constraint.
-        environment: Environment class which is equipped with sample() method to
-        return observed value.
-        beta (optional): Hyper-parameter to tune the exploration-exploitation
-        balance. If beta is large, it emphasizes the variance of the unexplored
-        solution solution (i.e. larger curiosity)
-        '''
+        """
+        This is adapted from the repo found at
+        https://github.com/tushuhei/gpucb
+
+        Arguments:
+            meshgrid - The parameter space on which to explore possible inputs.
+                Expected to be the output from np.meshgrid
+                type == list
+            environment - Environment class. Should have a 'sample' function.
+                type == class
+            beta - Hyper-parameter to tune the exploration-exploitation
+                balance. If beta is large, it emphasizes the variance of the
+                unexplored solution solution (i.e. larger curiosity)
+                default = 100
+                type == float
+
+        """
         self.meshgrid = np.array(meshgrid)
         self.environment = environment
         self.beta = beta
+        self.input_dimension = self.meshgrid.shape[0]
 
         # save param space as the transpose
-        self.X_grid = self.meshgrid.reshape(self.meshgrid.shape[0], -1).T
+        self.X_grid = self.meshgrid.reshape(self.input_dimension, -1).T
         # initialize means and sigmas
+        # TODO take in an optional prior for initialization
         self.mu = np.array([0. for _ in range(self.X_grid.shape[0])])
         self.sigma = np.array([0.5 for _ in range(self.X_grid.shape[0])])
-        # input params that have been tested
+        # input params that have been explored
         self.X = []
+        # outputs of the explored inputs
+        self.Y = []
         # time step
-        self.T = []
+        self.T = 0
 
     def argmax_ucb(self):
+        """Returns the argmax of the upper confidence bound."""
         return np.argmax(self.mu + self.sigma * np.sqrt(self.beta))
 
     def learn(self):
-        grid_idx = self.argmax_ucb()
-        self.sample(self.X_grid[grid_idx])
+        grid_index = self.argmax_ucb()
+        self.sample(self.X_grid[grid_index])
         gp = GaussianProcessRegressor()
-        gp.fit(self.X, self.T)
+        gp.fit(self.X, self.Y)
         self.mu, self.sigma = gp.predict(self.X_grid, return_std=True)
+        self.T = self.T + 1
 
     def sample(self, x):
-        t = self.environment.sample(x)
+        y = self.environment.sample(x)
         self.X.append(x)
-        self.T.append(t)
+        self.Y.append(y)
 
-    def plot(self):
-        fig = plt.figure()
-        ax = Axes3D(fig)
-        ax.plot_wireframe(self.meshgrid[0], self.meshgrid[1],
-            self.mu.reshape(self.meshgrid[0].shape), alpha=0.5, color='g')
-        ax.plot_wireframe(self.meshgrid[0], self.meshgrid[1],
-            self.environment.sample(self.meshgrid), alpha=0.5, color='b')
-        ax.scatter([x[0] for x in self.X], [x[1] for x in self.X], self.T, c='r',
-            marker='o', alpha=1.0)
-        # plt.savefig('fig_%02d.png' % len(self.X))
+
+class BatchGPUCB(GPUCB):
+
+    def __init__(self, batch_size, *args, **kwargs):
+        """
+        Child init function.
+
+        Arguments:
+            bacth_size - The number of trials in a single batch.
+                type == int
+        args:
+            meshgrid - The parameter space on which to explore possible inputs.
+                Expected to be the output from np.meshgrid
+                type == list
+            environment - Environment class. Should have a 'sample' function.
+                type == class
+        kwargs:
+            beta - Hyper-parameter to tune the exploration-exploitation
+                balance. If beta is large, it emphasizes the variance of the
+                unexplored solution solution (i.e. larger curiosity)
+                default = 100
+                type == float
+        """
+        # setting batch size attribute
+        self.batch_size = batch_size
+        # inherent everything else from parent class
+        super(BatchGPUCB, self).__init__(*args, **kwargs)
+
+
+    def argsort_ucb(self):
+        argsort_arr = np.flip(np.argsort(self.mu + self.sigma * np.sqrt(self.beta)))
+        return argsort_arr[:self.batch_size]
+
+    def learn(self):
+        # insert here "grid search" if self.T is 0
+        # currently, it takes the first in the parameter list
+        grid_indices = self.argsort_ucb()
+        self.batch_sample(self.X_grid[grid_indices])
+        # get ucb's from GP
+        gp = GaussianProcessRegressor()
+        # print(np.array(self.X).shape)
+        X = np.array(self.X).reshape(-1, self.input_dimension)
+        Y = np.array(self.Y).reshape(-1)
+        gp.fit(X, Y)
+        self.mu, self.sigma = gp.predict(self.X_grid, return_std=True)
+        # increase time step
+        self.T = self.T + 1
+
+    def batch_sample(self, xs):
+        ys = []
+        for x in xs:
+            y = self.environment.sample(x)
+            ys.append(y)
+        self.Y.append(ys)
+        self.X.append(xs)

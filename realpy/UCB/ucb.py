GPBUCB init uses np functions, fixed london vs latin HC, fixed off by one error in LHC, added v3 tests

realpy/UCB/ucb.py
@@ -8,12 +8,13 @@ Gaussian Process Bandit Optimization, Desautels, et. al, 2014.
 import numpy as np
 import sklearn.gaussian_process
 import smt.sampling_methods
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel
 
 
 class GPUCB(object):
     """Adapted from https://github.com/tushuhei/gpucb."""
 
-    def __init__(self, meshgrid, environment, beta=100.):
+    def __init__(self, meshgrid, environment, beta=100., kernel=None):
         """
         Init function.
 
@@ -37,13 +38,14 @@ class GPUCB(object):
         self.environment = environment
         self.beta = beta
         self.input_dimension = self.meshgrid.shape[0]
+        self.kernel = kernel
 
         # save param space as the transpose
         self.X_grid = self.meshgrid.reshape(self.input_dimension, -1).T
         # initialize means and sigmas
         # TODO take in an optional prior for initialization (e.g. a trained GP)
-        self.mu = np.array([0. for _ in range(self.X_grid.shape[0])])
-        self.sigma = np.array([0.5 for _ in range(self.X_grid.shape[0])])
+        self.mu = np.zeros(self.X_grid.shape[0])
+        self.sigma = np.ones(self.X_grid.shape[0])
         # input params that have been explored
         self.X = []
         # outputs of the explored inputs
@@ -70,7 +72,7 @@ class GPUCB(object):
         """
         grid_index = self.argmax_ucb()  # 1
         self.sample(self.X_grid[grid_index])  # 2
-        gp = sklearn.gaussian_process.GaussianProcessRegressor()
+        gp = sklearn.gaussian_process.GaussianProcessRegressor(kernel=self.kernel)
         gp.fit(self.X, self.Y)  # 3
         self.mu, self.sigma = gp.predict(self.X_grid, return_std=True)  # 4
         self.T = self.T + 1  # 5
@@ -140,12 +142,12 @@ class BatchGPUCB(GPUCB):
             5. Increases the time step.
         """
         if self.T == 0:
-            self.london_hypercube_sample()
+            self.latin_hypercube_sample()
         else:
             grid_indices = self.argsort_ucb()  # 1
             self.batch_sample(self.X_grid[grid_indices])  # 2
         # get ucb's from GP
-        gp = sklearn.gaussian_process.GaussianProcessRegressor()
+        gp = sklearn.gaussian_process.GaussianProcessRegressor(kernel=self.kernel)
         # reshape appropriately
         X = np.array(self.X).reshape(-1, self.input_dimension)
         Y = np.array(self.Y).reshape(-1)
@@ -154,6 +156,22 @@ class BatchGPUCB(GPUCB):
         # increase time step
         self.T = self.T + 1  # 5
         return None
+
+#     def batch_sample(self, xs):
+#         """
+#         Sample the sets of input in xs using the environment object.
+#         Save each input and output pair to the X and Y attributes,
+#         respectively.
+
+#         Arguments:
+#             xs - The set of input parameters to sample.
+#                 type == list or array of tuples
+#         """
+#         self.environment.write_actions(xs, self.T)
+#         ys = self.environment.sample()
+#         self.Y.append(ys)
+#         self.X.append(xs)
+#         return None
 
     def batch_sample(self, xs):
         """
@@ -165,12 +183,15 @@ class BatchGPUCB(GPUCB):
             xs - The set of input parameters to sample.
                 type == list or array of tuples
         """
-        ys = self.environment.sample(xs, time_step=self.T)
+        ys = []
+        for x in xs:
+            y = self.environment.sample(x)
+            ys.append(y)
         self.Y.append(ys)
-        self.X.append(np.array(xs))
+        self.X.append(xs)
         return None
 
-    def london_hypercube_sample(self):
+    def latin_hypercube_sample(self):
         """
         Does a London Hypercube (LH) sampling of the parameter space.
         LH based on the indexes of the parameter space. Use indices
@@ -179,9 +200,10 @@ class BatchGPUCB(GPUCB):
 
         limits = []
         for dim in self.meshgrid.shape[1:]:
-            limits.append([0, dim])
+            limits.append([0, dim-1])
+        
 
-        LH_sampler = smt.sampling_methods.LHS(xlimits=np.array(limits))
+        LH_sampler = smt.sampling_methods.LHS(xlimits=np.array(limits), random_state = 42)
         sampled = np.round(LH_sampler(self.batch_size)).astype(int)
         t = self.meshgrid.T
         xs = [t[tuple(sample)] for sample in sampled]
@@ -189,15 +211,14 @@ class BatchGPUCB(GPUCB):
         return None
 
 
-class HallucinateBatchGPUCB(BatchGPUCB):
+class BatchGPUCBv2(BatchGPUCB):
     """
-    Batched Guassian Process Upper Confidence Bound agent V2.
-    With GP regressions in-batch (i.e. "hallucinations").
-    Slower compute time, but should in general converge
-    in fewer batchs than V1.
+    Batched Guassian Process Upper Confidence Bound agent V2. With
+    GP regressions in-batch. Slower compute time, but should
+    in general converge in fewer batchs than V1.
     """
     def __init__(self, *args, **kwargs):
-        super(HallucinateBatchGPUCB, self).__init__(*args, **kwargs)
+        super(BatchGPUCBv2, self).__init__(*args, **kwargs)
         self.to_exclude = []
 
     def learn(self):
@@ -217,13 +238,13 @@ class HallucinateBatchGPUCB(BatchGPUCB):
         For the first timestep, a london hypercube sampling method is used
         """
         if self.T == 0:
-            self.london_hypercube_sample()
+            self.latin_hypercube_sample()
         else:
             best_idxs = []
             for i in range(self.batch_size):  # 1
                 best_idx = self.get_best_ucb()
                 best_idxs.append(best_idx.item())
-                gp = sklearn.gaussian_process.GaussianProcessRegressor()
+                gp = sklearn.gaussian_process.GaussianProcessRegressor(kernel=self.kernel)
                 self.false_sample(best_idx)
                 X = np.array(self.X).reshape(-1, self.input_dimension)
                 Y = np.array(self.Y).reshape(-1)
@@ -232,7 +253,7 @@ class HallucinateBatchGPUCB(BatchGPUCB):
 
             self.batch_sample(self.X_grid[best_idxs])  # 2
 
-        gp = sklearn.gaussian_process.GaussianProcessRegressor()  # 3
+        gp = sklearn.gaussian_process.GaussianProcessRegressor(kernel=self.kernel)  # 3
         X = np.array(self.X).reshape(-1, self.input_dimension)
         Y = np.array(self.Y).reshape(-1)
         gp.fit(X, Y)
@@ -253,11 +274,36 @@ class HallucinateBatchGPUCB(BatchGPUCB):
         """
         self.X = self.X[0:-self.batch_size]
         self.Y = self.Y[0:-self.batch_size]
-        ys = self.environment.sample(xs, time_step=self.T)
-        self.Y.append(ys)
-        self.X.append(xs)
+        ys = []
+        for x in xs:
+            y = self.environment.sample(x)
+            ys.append(y)
+            self.X.append(x)
+            self.Y.append(y)
         self.to_exclude = []
         return None
+
+#     def batch_sample(self, xs):
+#         """
+#         Forgots assumed samples from within batch.
+#         Then samples the sets of input in xs using the environment object.
+#         Save each input and output pair to the X and Y attributes,
+#         respectively. Resets to_exclude for the next batch.
+
+#         Arguments:
+#             xs - The set of input parameters to sample.
+#                 type == list or array of tuples
+#         """
+#         self.X = self.X[0:-self.batch_size]
+#         self.Y = self.Y[0:-self.batch_size]
+
+#         self.environment.write_actions(xs, self.T)
+#         ys = self.environment.sample()
+#         for x in xs:
+#             self.X.append(x)
+#         for y in ys:
+#             self.Y.append(y)
+#         return None
 
     def false_sample(self, i):
         """
@@ -265,8 +311,9 @@ class HallucinateBatchGPUCB(BatchGPUCB):
         respectively.
         """
         self.X.append(self.X_grid[i].tolist())
-        # self.X.append(self.X_grid[i])
         self.Y.append(self.mu[i])
+        
+        return None
 
     def get_best_ucb(self):
         """
@@ -279,6 +326,55 @@ class HallucinateBatchGPUCB(BatchGPUCB):
         idx = argsort_arr[i]
         while idx in self.to_exclude:
             i += 1
+            if i >= len(argsort_arr):
+                break
             idx = argsort_arr[i]
         self.to_exclude.append(idx)
         return idx
+    
+    
+class BatchGPUCBv3(BatchGPUCBv2):
+    """
+    Batched Guassian Process Upper Confidence Bound agent V2. With
+    GP regressions in-batch. Slower compute time, but should
+    in general converge in fewer batchs than V1.
+    """
+    def __init__(self, *args, **kwargs):
+        super(BatchGPUCBv3, self).__init__(*args, **kwargs)
+        
+    def batch_sample(self, xs):
+        """
+        Forgots assumed samples from within batch. Then samples the sets of
+        input in xs using the environment object.
+        Save each input and output pair to the X and Y attributes,
+        respectively. Resets to_exclude for the next batch.
+
+        Arguments:
+            xs - The set of input parameters to sample.
+                type == list or array of tuples
+        """
+        self.X = self.X[0:-self.batch_size]
+        self.Y = self.Y[0:-self.batch_size]
+        ys = []
+        for x in xs:
+            y = self.environment.sample(x)
+            ys.append(y)
+            self.X.append(x)
+            self.Y.append(y)
+        self.to_exclude = []
+        
+        # Prune 0 potential points in parameter space
+        # If the best lcb is higher than a points ucb, we
+        # no longer need to regress over that point
+        # But don't want to remove information so keep points
+        # In the space that we have sampled at
+        threshold = np.max(self.mu - self.sigma)
+        args = np.argwhere((self.mu+self.sigma*np.sqrt(self.beta)) >= threshold)
+        self.X_grid = self.X_grid[args.squeeze()]
+        if len(self.X_grid) < self.batch_size:
+            self.batch_size = len(self.X_grid)
+ 
+        
+        return None
+    
+    

realpy/tests/test_UCB/test_ucb.py
@@ -58,7 +58,7 @@ class TestGPUCB(unittest.TestCase):
         return_val = subject.argmax_ucb()
         self.assertEqual(return_val, 5)
         # making the max occur at index 10 by adjusting the std
-        subject.sigma[10] = 2.
+        subject.sigma[10] = 3
         return_val = subject.argmax_ucb()
         self.assertEqual(return_val, 10)
 
@@ -182,11 +182,10 @@ class TestBatchGPUCB(unittest.TestCase):
 
         indices = [2, 4, 5]
         subject.batch_sample(indices)
-        self.assertEqual(subject.Y[0], "Batch")
+        self.assertEqual(subject.Y[0], ["Batch", "Batch", "Batch"])
 
     @unittest.mock.patch('smt.sampling_methods.LHS')
-    def test_london_hypercube_sample(self, mocked_lhs):
-        """Test Latin Hypercube (LH) sampling."""
+    def test_latin_hypercube_sample(self, mocked_lhs):
         mocked_env = mock.MagicMock(name='env')
         mocked_env.sample = mock.MagicMock(name='env_sample',
                                            return_value="Batch")
@@ -201,20 +200,20 @@ class TestBatchGPUCB(unittest.TestCase):
         coeffs = np.arange(n)
         meshgrid = np.meshgrid(coeffs, coeffs)
         subject = ucb.BatchGPUCB(batch_size, meshgrid, mocked_env, beta=1)
-        subject.london_hypercube_sample()
+        subject.latin_hypercube_sample()
         self.assertEqual(list(subject.X[0][0]), [3, 2])
         self.assertEqual(list(subject.X[0][1]), [0, 1])
         self.assertEqual(list(subject.X[0][2]), [0, 2])
         name, args, kwargs = mocked_lhs.mock_calls[0]
-        self.assertEqual(kwargs['xlimits'].tolist(), [[0, n], [0, n]])
+        self.assertEqual(kwargs['xlimits'].tolist(), [[0, n-1], [0, n-1]])
 
 
-class TestHallucinateBatchGPUCB(unittest.TestCase):
+class TestBatchGPUCBv2(unittest.TestCase):
     """Test Batch GP UCBv2 class."""
 
     def test_inheritence(self):
         """Ensure the subclass class inherits from parent class."""
-        self.assertTrue(issubclass(ucb.HallucinateBatchGPUCB, ucb.BatchGPUCB))
+        self.assertTrue(issubclass(ucb.BatchGPUCBv2, ucb.BatchGPUCB))
 
     def test___init__(self):
         """Test initialization of the Batch GP UCBv2 class."""
@@ -224,7 +223,7 @@ class TestHallucinateBatchGPUCB(unittest.TestCase):
         batch_size = 3
         coeffs = np.arange(n)
         meshgrid = np.meshgrid(coeffs, coeffs)
-        subject = ucb.HallucinateBatchGPUCB(batch_size, meshgrid, mocked_env)
+        subject = ucb.BatchGPUCBv2(batch_size, meshgrid, mocked_env, beta=1)
         # test assignment of additional attribute
         self.assertTrue(hasattr(subject, 'batch_size'))
         self.assertEqual(subject.batch_size, batch_size)
@@ -238,7 +237,7 @@ class TestHallucinateBatchGPUCB(unittest.TestCase):
         batch_size = 3
         coeffs = np.arange(n)
         meshgrid = np.meshgrid(coeffs, coeffs)
-        subject = ucb.HallucinateBatchGPUCB(batch_size, meshgrid, mocked_env)
+        subject = ucb.BatchGPUCBv2(batch_size, meshgrid, mocked_env, beta=1)
         # making the max occur at starting at index 2 for an entire batch
         subject.mu[2:2 + batch_size] = 1
         subject.mu[3] = 10
@@ -260,7 +259,7 @@ class TestHallucinateBatchGPUCB(unittest.TestCase):
         batch_size = 3
         coeffs = np.arange(n)
         meshgrid = np.meshgrid(coeffs, coeffs)
-        subject = ucb.HallucinateBatchGPUCB(batch_size, meshgrid, mocked_env)
+        subject = ucb.BatchGPUCBv2(batch_size, meshgrid, mocked_env, beta=1)
 
         # set up mocked functions
         subject.batch_sample = mock.MagicMock(name='batch sample')
@@ -285,23 +284,62 @@ class TestHallucinateBatchGPUCB(unittest.TestCase):
         batch_size = 3
         coeffs = np.arange(n)
         meshgrid = np.meshgrid(coeffs, coeffs)
-        subject = ucb.HallucinateBatchGPUCB(batch_size, meshgrid, mocked_env)
+        subject = ucb.BatchGPUCBv2(batch_size, meshgrid, mocked_env, beta=1)
         subject.to_exclude.append(1)
 
         indices = [2, 4, 5]
         subject.batch_sample(indices)
-        self.assertEqual(subject.Y, ["Batch"])
+        self.assertEqual(subject.Y, ["Batch", "Batch", "Batch"])
         self.assertEqual(subject.to_exclude, [])
 
     def test_false_sample(self):
-        """Test false sample."""
         mocked_env = mock.MagicMock(name='env',
                                     return_value="Batch")
         n = 5
         batch_size = 3
         coeffs = np.arange(n)
         meshgrid = np.meshgrid(coeffs, coeffs)
-        subject = ucb.HallucinateBatchGPUCB(batch_size, meshgrid, mocked_env)
+        subject = ucb.BatchGPUCBv2(batch_size, meshgrid, mocked_env, beta=1)
         subject.false_sample(0)
         self.assertEqual(subject.X[-1], [0, 0])
         self.assertEqual(subject.Y[-1], 0)
+        
+        
+        
+class TestBatchGPUCBv3(unittest.TestCase):
+    """Test Batch GP UCBv2 class."""
+
+    def test_inheritence(self):
+        """Ensure the subclass class inherits from parent class."""
+        self.assertTrue(issubclass(ucb.BatchGPUCBv3, ucb.BatchGPUCBv2))
+
+    def test___init__(self):
+        """Test initialization of the Batch GP UCBv3 class."""
+        # initialize
+        mocked_env = mock.MagicMock(name='env')
+        n = 5
+        batch_size = 3
+        coeffs = np.arange(n)
+        meshgrid = np.meshgrid(coeffs, coeffs)
+        subject = ucb.BatchGPUCBv3(batch_size, meshgrid, mocked_env, beta=1)
+        # test assignment of additional attribute
+        self.assertTrue(hasattr(subject, 'batch_size'))
+        self.assertEqual(subject.batch_size, batch_size)
+
+
+    def test_batch_sample(self):
+        """Test the environment sampling."""
+        mocked_env = mock.MagicMock(name='env')
+        mocked_env.sample = mock.MagicMock(name='env_sample',
+                                           return_value="Batch")
+        n = 5
+        batch_size = 3
+        coeffs = np.arange(n)
+        meshgrid = np.meshgrid(coeffs, coeffs)
+        subject = ucb.BatchGPUCBv2(batch_size, meshgrid, mocked_env, beta=1)
+        subject.to_exclude.append(1)
+
+        indices = [2, 4, 5]
+        subject.batch_sample(indices)
+        self.assertEqual(subject.Y, ["Batch", "Batch", "Batch"])
+        self.assertEqual(subject.to_exclude, [])